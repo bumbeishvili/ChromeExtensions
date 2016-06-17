c9300243636f427cc7890cf2249ba02e535741f1
dato <dato39507@yahoo.com>
2016-06-17 10:04:10 +0400

    style button, add name and description

--- Extensions/EnableDisabled/Content.js
+++ Extensions/EnableDisabled/Content.js
@@ -1,16 +1,35 @@
 ﻿
 addButton();
 function addButton(){
+  
+
 var element = document.createElement("input");
 
 element.type = 'button';
-element.value = 'Enable';
-element.name = 'test';
+
 element.style.position = "fixed";
 element.style.zIndex = "9999";
 element.style.right = "0";
-element.classList.add('btn');
-element.classList.add('btn-default');
+element.style.top = "0";
+
+element.style.backgroundColor='#9C8C42';
+  element.style.border= '2px solid #91782c';
+  element.style.color='#FFF';
+  element.style.textAalign='center';
+  element.style.borderRradius= '4px';
+  element.style.padding= '3px'; 
+  element.style.cursor= 'pointer'; 
+  
+ 
+ 
+   element.value = "Enable All";
+   element.style.opacity='0';
+
+ 
+
+
+
+
 element.onclick = function() {
   var selectors = ['button', 'input', 'textarea', 'select', 'li', 'a','fieldset','div','img'];
   selectors.forEach(function(selector) {
